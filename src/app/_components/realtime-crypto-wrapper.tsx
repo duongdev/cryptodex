10a013e90f3d388be280aef4544fd8c620a56ce5
Dustin Do <dustin.do95@gmail.com>
2024-03-15 01:18:11 +0700

    feat: add indicator

--- src/app/_components/realtime-crypto-wrapper.tsx
+++ src/app/_components/realtime-crypto-wrapper.tsx
@@ -7,6 +7,7 @@ import { FC, useEffect, useState } from 'react'
 import { CryptoBubbles } from './crypto-bubbles'
 import { CryptoDataTable } from './crypto-table/crypto-table'
 import { columns } from './crypto-table/columns'
+import { Progress } from '@/components/ui/progress'
 
 export type RealtimeCryptoWrapperProps = {
   initialCryptos: CryptoData[]
@@ -18,9 +19,11 @@ export const RealtimeCryptoWrapper: FC<RealtimeCryptoWrapperProps> = ({
   top,
 }) => {
   const [cryptos, setCryptos] = useState<CryptoData[]>(initialCryptos)
+  const [lastUpdatedAt, setLastUpdatedAt] = useState(Date.now())
 
   useEffect(() => {
     const interval = setInterval(async () => {
+      setLastUpdatedAt(Date.now())
       getCryptoData(top).then(setCryptos)
     }, REFRESH_INTERVAL)
 
@@ -29,6 +32,10 @@ export const RealtimeCryptoWrapper: FC<RealtimeCryptoWrapperProps> = ({
 
   return (
     <>
+      <UpdateProgress
+        lastUpdatedAt={lastUpdatedAt}
+        updateInterval={REFRESH_INTERVAL}
+      />
       <CryptoBubbles
         cryptos={cryptos}
         className="h-[calc(100dvh-56px)] bg-slate-900"
@@ -37,5 +44,28 @@ export const RealtimeCryptoWrapper: FC<RealtimeCryptoWrapperProps> = ({
         <CryptoDataTable columns={columns} data={cryptos} />
       </div>
     </>
+  )
+}
+
+const UpdateProgress: FC<{ lastUpdatedAt: number; updateInterval: number }> = ({
+  lastUpdatedAt,
+  updateInterval,
+}) => {
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const elapsed = Date.now() - lastUpdatedAt
+      setProgress((elapsed / updateInterval) * 100)
+    }, 200)
+
+    return () => clearInterval(interval)
+  }, [lastUpdatedAt, updateInterval])
+
+  return (
+    <Progress
+      value={progress}
+      className="h-1 rounded-none [&>.indicator]:duration-200"
+    />
   )
 }
