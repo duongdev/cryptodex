665c6a6cf2ecc0b97af95fce486f6530e2cde78a
Dustin Do <dustin.do95@gmail.com>
2024-03-31 14:24:01 +0700

    feat: add vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
