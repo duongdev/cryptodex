984d6bdfce9d2e27d21984a049a2747ab2c99dc5
Dustin Do <dustin.do95@gmail.com>
2024-04-03 16:22:08 +0700

    feat(cms): integrate with vercel blob storage

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'https://kyemju3fys49yd5q.public.blob.vercel-storage.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'https://kyemju3fys49yd5q.public.blob.vercel-storage.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@schornio/strapi-provider-upload-vercel-blob',
+      providerOptions: {
+        token: env('BLOB_READ_WRITE_TOKEN'),
+      },
+    },
+  },
+});
