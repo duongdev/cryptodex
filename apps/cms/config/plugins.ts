92127bbcad2aeb7fefab76a09d7a1ca06814c6ee
Dustin Do <dustin.do95@gmail.com>
2024-04-03 17:01:38 +0700

    feat(cms): add documentation plugin

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -7,4 +7,36 @@ export default ({ env }) => ({
       },
     },
   },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'DOCUMENTATION',
+        description: '',
+        termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+        contact: {
+          name: 'TEAM',
+          email: 'contact-email@something.io',
+          url: 'mywebsite.io'
+        },
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        },
+      },
+      'x-strapi-config': {
+        // Leave empty to ignore plugins during generation
+        plugins: [ 'upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [{ url: 'http://localhost:1337/api', description: 'Development server' }],
+      externalDocs: {
+        description: 'Find out more',
+        url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
+      },
+      security: [ { bearerAuth: [] } ]
+    }
+  }
 });
