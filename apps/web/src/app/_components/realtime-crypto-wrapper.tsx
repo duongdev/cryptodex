c48eeda97adcf8d1d174400598d3caf77b3b6caf
Dustin Do <dustin.do95@gmail.com>
2024-04-10 00:17:41 +0700

    chore(web): make the table wider

--- apps/web/src/app/_components/realtime-crypto-wrapper.tsx
+++ apps/web/src/app/_components/realtime-crypto-wrapper.tsx
@@ -64,7 +64,7 @@ export const RealtimeCryptoWrapper: FC<RealtimeCryptoWrapperProps> = ({
         cryptos={topCryptos}
       />
       <div
-        className="container -mt-10 flex flex-col gap-4 px-2 pb-8 pt-16 md:px-8"
+        className="container max-w-screen-2xl -mt-10 flex flex-col gap-4 px-2 pb-8 pt-16 md:px-8"
         id="table-wrapper"
       >
         {belowBubblesBanners.length > 0 && (
