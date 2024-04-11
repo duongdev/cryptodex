e104d64e885ed19c877e9515e969dbadb113472b
Dustin Do <dustin.do95@gmail.com>
2024-04-12 01:47:38 +0700

    fix(web): fix about us page

--- apps/web/src/app/[...pageSlug]/page.tsx
+++ apps/web/src/app/[...pageSlug]/page.tsx
@@ -17,7 +17,7 @@ export default async function Page({
   }
 
   return (
-    <div>
+    <div className="relative min-h-dvh">
       <div className="absolute top-0 -z-10 h-full w-full bg-white dark:hidden">
         <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
       </div>
