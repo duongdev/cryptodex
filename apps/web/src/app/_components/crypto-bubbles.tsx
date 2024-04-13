91bf8b109dddfaa2b2789696519e0d039a21b645
Dustin Do <dustin.do95@gmail.com>
2024-04-14 01:27:53 +0700

    feat(web): improve bubbles

--- apps/web/src/app/_components/crypto-bubbles.tsx
+++ apps/web/src/app/_components/crypto-bubbles.tsx
@@ -10,7 +10,7 @@ import type { HighchartsReactRefObject } from 'highcharts-react-official'
 import HighchartsReact from 'highcharts-react-official'
 import { useAtom } from 'jotai'
 import numeral from 'numeral'
-import { useWindowSize } from 'react-use'
+import { useWindowSize, useSize, useMeasure } from 'react-use'
 
 import { exchangeFilterAtom, performanceOptionAtom } from '@/atoms/crypto'
 import { useTrackBannerClick } from '@/components/ad-banner'
@@ -47,8 +47,15 @@ export const CryptoBubbles: FC<CryptoBubblesProps> = ({
     PERFORMANCE_OPTIONS[performanceOption] ||
     PERFORMANCE_OPTIONS[DEFAULT_PERFORMANCE_OPTION]
   const performanceKey = selectedPerformanceOption.key
+  // const performanceKey = 'y'
 
-  const { width, height } = useWindowSize()
+  const { width: ww, height: wh } = useWindowSize()
+  const [ref, { width: mw, height: mh }] = useMeasure<HTMLDivElement>()
+
+  const width = mw || ww
+  const height = mh || wh
+
+  console.log('🚀 ~ x:', { width, height })
 
   const maxAbsValue = useMemo(() => {
     return Math.max(
@@ -86,175 +93,204 @@ export const CryptoBubbles: FC<CryptoBubblesProps> = ({
     )
   }, [cryptos, performanceKey])
 
-  const { minSize, maxSize } = useMemo(() => {
-    const [baseMinSize, baseMaxSize] = selectedPerformanceOption.sizes
+  const bubbleSizes = useMemo(() => {
+    const bubbleValues = cryptos.map((crypto) => ({
+      symbol: crypto.symbol,
+      value: Math.abs((crypto.performance as ANY)[performanceKey] as number),
+    }))
 
-    const s = width * height
+    // Constants
+    const s = width * height // Area of the chart in pixels
+    const minSize = 30 // Min size is 30 pixels
+    const maxSizeMultiplier = 3.5 // Max size is 3.5 times the min size
+    const chartArea = Math.pow(Math.min(width, height), 2) * 0.9
 
-    /* For s=1200000, min=50, max=250. Scales the min and max by s */
-    // eslint-disable-next-line @typescript-eslint/no-shadow
-    const minSize = Math.sqrt(
-      (baseMinSize * baseMinSize * s) / BUBBLE_OPTIONS.baseSquareSize,
-    )
-    // eslint-disable-next-line @typescript-eslint/no-shadow
-    const maxSize = Math.sqrt(
-      (baseMaxSize * baseMaxSize * s) / BUBBLE_OPTIONS.baseSquareSize,
-    )
+    // Normalize values: Convert each value to a scale where the smallest value is assigned the minSize,
+    // and the largest does not exceed maxSizeMultiplier times the minSize.
+    let minValue = Math.min(...bubbleValues.map((b) => b.value))
+    let maxValue = Math.max(...bubbleValues.map((b) => b.value))
 
-    // const minSize = 20
-    // const maxSize = 120
+    // Adjust for relative scaling for values >= 200
+    const adjustedValues = bubbleValues.map((b) => ({
+      symbol: b.symbol,
+      value:
+        b.value >= 200
+          ? 200 + Math.log(b.value + 1 - 200) * (maxValue / 200)
+          : b.value,
+    }))
 
-    return { minSize, maxSize }
-  }, [height, selectedPerformanceOption.sizes, width])
+    minValue = Math.min(...adjustedValues.map((b) => b.value))
+    maxValue = Math.max(...adjustedValues.map((b) => b.value))
 
-  const options: Highcharts.Options = useMemo(() => {
-    return {
-      chart: {
-        type: 'packedbubble',
-        backgroundColor: 'transparent',
-      },
-      title: false as ANY,
-      tooltip: false as ANY,
-      legend: {
-        enabled: false,
-      },
-      credits: {
-        enabled: false,
-      },
-      plotOptions: {
-        packedbubble: {
-          draggable: true,
-          cursor: 'pointer',
+    // Calculate total area needed for all bubbles without scaling
+    let totalArea = 0
+    const baseAreas = adjustedValues.map((b) => {
+      const relativeSize = (b.value - minValue) / (maxValue - minValue)
+      const diameter =
+        minSize + (maxSizeMultiplier * minSize - minSize) * relativeSize
+      const area = Math.PI * (diameter / 2) ** 2
+      // const area = (diameter) ** 2
+      totalArea += area
+      return { symbol: b.symbol, area }
+    })
+
+    // Scale factor to fit all bubbles within 90% of the chart area
+    const scaleFactor = Math.sqrt(chartArea / totalArea)
+
+    // Calculate final sizes
+    const sizes: Record<string, number> = {}
+    baseAreas.forEach((b) => {
+      const diameter = Math.sqrt((b.area * scaleFactor) / Math.PI) * 2 // Reverse area calculation to get diameter
+      sizes[b.symbol] = diameter // Assign and round off the diameter
+    })
+
+    return sizes
+  }, [height, width, cryptos, performanceKey])
+
+  console.log(bubbleSizes)
+
+  const minSize = Math.min(...Object.values(bubbleSizes))
+  const maxSize = Math.max(...Object.values(bubbleSizes))
+  console.log(minSize, maxSize)
+
+  const options: Highcharts.Options = {
+    chart: {
+      type: 'packedbubble',
+      backgroundColor: 'transparent',
+    },
+    title: false as ANY,
+    tooltip: false as ANY,
+    legend: {
+      enabled: false,
+    },
+    credits: {
+      enabled: false,
+    },
+    plotOptions: {
+      packedbubble: {
+        draggable: true,
+        cursor: 'pointer',
+        animation: !true,
+        minSize,
+        maxSize,
+        // sizeBy: 'size',
+        // zMin: 0,
+        // zMax: 100,
+        layoutAlgorithm: BUBBLE_OPTIONS.layoutAlgorithm,
+        events: {
+          click(e) {
+            const point = e.point as ANY
+            if (!point?.options?.banner) {
+              return
+            }
+            if (typeof point?.options?.link === 'string') {
+              trackBannerClick(point.options?.banner)
+              window.open(point?.options?.link, '_blank')
+            }
+          },
+        },
+        dataLabels: {
+          useHTML: true,
+          enabled: true,
           animation: !true,
-          minSize: `${minSize}px`,
-          maxSize: `${maxSize}px`,
-          zMin: 0,
-          zMax: 100,
-          layoutAlgorithm: BUBBLE_OPTIONS.layoutAlgorithm,
-          events: {
-            click(e) {
-              const point = e.point as ANY
-              if (!point?.options?.banner) {
-                return
-              }
-              if (typeof point?.options?.link === 'string') {
-                trackBannerClick(point.options?.banner)
-                window.open(point?.options?.link, '_blank')
-              }
-            },
+          formatter() {
+            // eslint-disable-next-line react/no-this-in-sfc
+            const point = this.point as ANY
+            const isBanner = !!point.options?.banner
+            // scale font size based on value
+            // const size =
+            //   Math.max(
+            //     minSize,
+            //     Math.min(
+            //       maxSize,
+            //       (point.value / 100) * (maxSize - minSize) + minSize,
+            //     ),
+            //   ) * 0.2
+            const size = 0.2 * point.value
+            return `
+              <div style="text-align: center; width: 100%; display: grid; place-items: center; margin-bottom: 4px">
+                <img src="${point.image}" width="${size + 10}" height="${size + 10}"/>
+              </div>
+              <div style="font-size: ${size}px">
+                <b>${point.name}</b>
+                ${isBanner ? '' : `<br/>${numeral(point.orgValue).format((Math.abs(point.orgValue) > 10000 && '0') || (Math.abs(point.orgValue) > 100 && '0.0') || '0.00')}%`}
+              </div>
+            `
           },
-          dataLabels: {
-            useHTML: true,
-            enabled: true,
-            animation: !true,
-            formatter() {
-              // eslint-disable-next-line react/no-this-in-sfc
-              const point = this.point as ANY
-              const isBanner = !!point.options?.banner
-              // scale font size based on value
-              const size =
-                Math.max(
-                  minSize,
-                  Math.min(
-                    maxSize,
-                    (point.value / 100) * (maxSize - minSize) + minSize,
-                  ),
-                ) * 0.2
-              return `
-                <div style="text-align: center; width: 100%; display: grid; place-items: center; margin-bottom: 4px">
-                  <img src="${point.image}" width="${size + 10}" height="${size + 10}"/>
-                </div>
-                <div style="font-size: ${size}px">
-                  <b>${point.name}</b>
-                  ${isBanner ? '' : `<br/>${numeral(point.orgValue).format(point.orgValue > 100 ? '0.0' : '0.00')}%`}
-                </div>
-              `
-            },
-            style: {
-              color: 'white',
-              textOutline: 'none',
-              fontWeight: 'normal',
-              textAlign: 'center',
-            },
+          style: {
+            color: 'white',
+            textOutline: 'none',
+            fontWeight: 'normal',
+            textAlign: 'center',
           },
         },
       },
-      series: [
-        {
-          keys: ['name'],
-          data: [
-            ...cryptos.map((crypto) => {
-              const orgValue =
-                Math.round(
-                  ((crypto.performance as ANY)[performanceKey] as number) * 100,
-                ) / 100
-              const value = Math.abs(orgValue)
-              const isInExchangeFilter =
-                selectedExchanges.length === 0 ||
-                crypto.exchanges?.some((exchange) =>
-                  selectedExchanges.includes(exchange),
-                )
+    },
+    series: [
+      {
+        keys: ['name'],
+        data: [
+          ...cryptos.map((crypto, index) => {
+            const orgValue =
+              Math.round(
+                ((crypto.performance as ANY)[performanceKey] as number) * 100,
+              ) / 100
+            const value = Math.abs(orgValue)
+            const isInExchangeFilter =
+              selectedExchanges.length === 0 ||
+              crypto.exchanges?.some((exchange) =>
+                selectedExchanges.includes(exchange),
+              )
 
-              const {
-                minOpacity,
-                maxOpacity,
-                filteredOutOpacity,
-                positiveColor,
-                negativeColor,
-              } = BUBBLE_OPTIONS
+            const {
+              minOpacity,
+              maxOpacity,
+              filteredOutOpacity,
+              positiveColor,
+              negativeColor,
+            } = BUBBLE_OPTIONS
 
-              // scale opacity based on value
-              // if value is negative, scale based on minNegativeValue and maxNegativeValue
-              // if value is positive, scale based on minPositiveValue and maxPositiveValue
-              const opacity =
-                (!isInExchangeFilter && filteredOutOpacity) ||
-                (value < 0
-                  ? minOpacity +
-                    ((Math.abs(orgValue) - minNegativeValue) /
-                      (maxNegativeValue - minNegativeValue)) *
-                      (maxOpacity - minOpacity)
-                  : minOpacity +
-                    ((Math.abs(orgValue) - minPositiveValue) /
-                      (maxPositiveValue - minPositiveValue)) *
-                      (maxOpacity - minOpacity))
+            const size = bubbleSizes[crypto.symbol]
 
-              return {
-                name: crypto.symbol.toUpperCase(),
-                value,
-                orgValue,
-                image: crypto.image,
-                color:
-                  crypto.performance.d < 0
-                    ? `rgba(${negativeColor}, ${opacity})`
-                    : `rgba(${positiveColor}, ${opacity})`,
-              }
-            }),
-            ...banners.map((banner) => ({
-              banner,
-              name: banner.title_en,
-              image: banner.medium?.data?.attributes?.url,
-              color: 'rgb(77 11 218 / 1)',
-              value: maxAbsValue * 1.25,
-              link: banner.link?.external_url,
-            })),
-          ],
-        },
-      ] as ANY,
-    }
-  }, [
-    banners,
-    cryptos,
-    maxAbsValue,
-    maxNegativeValue,
-    maxPositiveValue,
-    maxSize,
-    minNegativeValue,
-    minPositiveValue,
-    minSize,
-    performanceKey,
-    selectedExchanges,
-    trackBannerClick,
-  ])
+            // scale opacity based on value
+            // if value is negative, scale based on minNegativeValue and maxNegativeValue
+            // if value is positive, scale based on minPositiveValue and maxPositiveValue
+            const opacity =
+              (!isInExchangeFilter && filteredOutOpacity) ||
+              (value < 0
+                ? minOpacity +
+                  ((Math.abs(orgValue) - minNegativeValue) /
+                    (maxNegativeValue - minNegativeValue)) *
+                    (maxOpacity - minOpacity)
+                : minOpacity +
+                  ((Math.abs(orgValue) - minPositiveValue) /
+                    (maxPositiveValue - minPositiveValue)) *
+                    (maxOpacity - minOpacity))
+
+            return {
+              name: crypto.symbol.toUpperCase(),
+              value: size,
+              orgValue,
+              image: crypto.image,
+              color:
+                orgValue < 0
+                  ? `rgba(${negativeColor}, ${opacity})`
+                  : `rgba(${positiveColor}, ${opacity})`,
+              size: `${size}px`,
+            }
+          }),
+          ...banners.map((banner) => ({
+            banner,
+            name: banner.title_en,
+            image: banner.medium?.data?.attributes?.url,
+            color: 'rgb(77 11 218 / 1)',
+            value: maxSize * 0.9,
+            link: banner.link?.external_url,
+          })),
+        ],
+      },
+    ] as ANY,
+  }
 
   useEffect(() => {
     logger.info('redraw chart')
@@ -262,7 +298,7 @@ export const CryptoBubbles: FC<CryptoBubblesProps> = ({
   }, [performanceOption])
 
   return (
-    <div className={className}>
+    <div className={className} ref={ref}>
       <HighchartsReact
         containerProps={{ style: { height: '100%' } }}
         highcharts={Highcharts}
